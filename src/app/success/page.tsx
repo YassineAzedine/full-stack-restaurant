9a4704c0296900d6472ea4966399b6eb5c6b01cb
YassineAzedine <yassine.azedine0711@gmail.com>
2025-03-21 16:52:07 +0000

    make change

--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -1,41 +1,39 @@
 "use client";
 import { useRouter, useSearchParams } from "next/navigation";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import ConfettiExplosion from "react-confetti-explosion";
 
 const SuccessPage = () => {
   const router = useRouter();
+  const [loading, setLoading] = useState(true);
   const searchParams = useSearchParams();
-  const payment_intent = searchParams.get("payment_intent");
+  const payment_intent = searchParams.get("payment_intent"); // Utilisation de useSearchParams pour obtenir les paramètres de l'URL
 
   useEffect(() => {
     const makeRequest = async () => {
-      try {
-        await fetch(`${process.env.NEXTAUTH_URL}/confirm/${payment_intent}`, {
-          method: "PUT",
-        });
-        setTimeout(() => {
-          router.push("/orders");
-        }, 5000);
-      } catch (err) {
-        console.log(err);
+      if (payment_intent) {
+        try {
+          await fetch(`${process.env.NEXTAUTH_URL}/api/confirm/${payment_intent}`, {
+            method: "PUT",
+          });
+          setTimeout(() => {
+            router.push("/orders");
+          }, 5000);
+        } catch (err) {
+          console.log(err);
+        }
       }
     };
 
-    makeRequest();
+    if (payment_intent) {
+      makeRequest();
+    }
   }, [payment_intent, router]);
 
   return (
-    <>
-      <div className="min-h-[calc(100vh-6rem)] md:min-h-[calc(100vh-15rem)] flex items-center justify-center text-center text-2xl text-green-700">
-        <p className="max-w-[600px]">
-          Payment successful. You are being redirected to the orders page.
-          Please do not close the page.
-        </p>
-      <ConfettiExplosion className="absolute m-auto"
-      />
-      </div>
-    </>
+    <div>
+      {loading ? <p>Processing payment...</p> : <p>Payment successful!</p>}
+    </div>
   );
 };
 
